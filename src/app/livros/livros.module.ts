3e7afff4ddbeed6c5d649464f9252b9453f572ec
MuriStanger <murielzanellato@gmail.com>
2021-05-05 19:28:18 -0300

    funcao de livros

--- src/app/livros/livros.module.ts
+++ src/app/livros/livros.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,11 +8,17 @@ import { LivrosPageRoutingModule } from './livros-routing.module';
 
 import { LivrosPage } from './livros.page';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
+    CommonModule,  
+    ReactiveFormsModule,
     IonicModule,
+    HttpClientModule ,
     LivrosPageRoutingModule
   ],
   declarations: [LivrosPage]
--- src/app/livros/livros.page.ts
+++ src/app/livros/livros.page.ts
@@ -1,4 +1,7 @@
+import { LivroService } from './livro.service';
+import { AlertController, ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { Livro } from './livro';
 
 @Component({
   selector: 'app-livros',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivrosPage implements OnInit {
 
-  constructor() { }
+  livros:Livro[];
+
+  constructor(
+    private alertController: AlertController,
+    private toastController:ToastController,
+    private livroService:LivroService) {
+    this.refreshLivros();
+  }
 
   ngOnInit() {
   }
 
+  async removeConfirm(livro:Livro){
+    await this.alertController.create({
+      header:'Confirmação de exclusão',
+      message:`Desenha excluir o livro ${livro.titulo}`,
+      buttons:[{
+        text:'Sim',
+        cssClass:'ion-color-success',
+        handler:() => {
+          this.remove(livro);
+        }
+      },{
+        text:'Não',
+        cssClass:'ion-color-danger',
+      }]
+    }).then(alert => alert.present());
+  }
+
+
+  remove(livro:Livro) {
+    this.livroService.removeLivro(livro).subscribe(
+      (value)=> this.refreshLivros(),
+      (error)=> {
+        this.toastController.create({
+          message: 'Não foi possivel excluir o livro',
+          duration: 5000,
+          keyboardClose: true,
+          color:'danger'
+        }).then(
+          t => t.present()
+        )}
+    );
+  }
+  refreshLivros(){
+     this.livroService.getLivros().subscribe(
+      (value)=> this.livros = value,
+      (error)=> console.error(error),
+    );
+    console.log(this.livros);
+  }
+
+  ionViewWillEnter(){
+    this.refreshLivros();
+  }
 }
